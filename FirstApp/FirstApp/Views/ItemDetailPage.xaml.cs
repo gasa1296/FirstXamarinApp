82701e935d02c6d8f6efb7d32873d3db4d7008aa
gabri <gabri@GAALSEAN>
2022-04-25 00:25:35 -0400

    Agregar archivos de proyecto.

---
+++ FirstApp/FirstApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FirstApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FirstApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ FirstApp/FirstApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using FirstApp.Models;
+using FirstApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
